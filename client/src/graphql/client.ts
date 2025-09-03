310293478fae49e7416c735fd2d6eecffd57575f
Alex Cocemasov <a.cocemasov@gmail.com>
2025-09-03 19:37:33 +0300

    Apollo client uri as env variable

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+    link: new HttpLink({ uri: import.meta.env.VITE_GRAPHQL_URI }),
     cache: new InMemoryCache(),
 })
 

9d55f2b9223962231806f942adfd5970d5e46cfc
Alex Cocemasov <a.cocemasov@gmail.com>
2025-09-03 18:34:30 +0300

    Adjust server start console message

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ async function startServer() {
 
     const port = process.env.PORT || 4000
     app.listen(port, () => {
-        console.log(`🚀 Server ready at http://localhost:${port}/graphql`)
+        console.log(`🚀 Server ready at port ${port}`)
     })
 }
 
